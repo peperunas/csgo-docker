18c6a57a2f3f01153a10d5810f6b8c24a8a2de51
Giulio De Pasquale <giulio@depasquale.eu>
2016-02-25 13:17:13 +0100

    Fixed data container permissions

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,10 @@ CMD ["/sbin/my_init"]
 # APPS INSTALL && CONFIG
 RUN apt-get update && apt-get install -y lib32gcc1 wget
 RUN chmod +x /docker/install_steamcmd.sh && useradd -m steam
+RUN mkdir -p /home/steam/csgo/csgo/cfg 
+RUN chown steam:steam /home/steam -R
 
-# fix locally mounted directory permissions
-RUN chown steam:steam /home/steam/ -R
+VOLUME /home/steam/csgo/csgo/cfg
 
 USER steam
 CMD /docker/install_steamcmd.sh
--- install_steamcmd.sh
+++ install_steamcmd.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+STEAMCMD_FILE=steamcmd_installer
 mkdir ~/steamcmd
 cd ~/steamcmd
-wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-tar -xvzf steamcmd_linux.tar.gz
+wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz -O $STEAMCMD_FILE
+tar -xvzf $STEAMCMD_FILE
 # installing csgo dedicated server
 ~/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/csgo +app_update 740 validate +quit
 # fixing popd/pushd
 sed -i "s/\#\!\/bin\/sh/\#\!\/bin\/bash/" ~/csgo/srcds_run
+# removing install file
+rm $STEAMCMD_FILE
 # executing server
 echo "Executing server."
 cd ~/csgo
